b22242867309a104b4e3cd09e65484124b77c331
Anshika Sharma <anshika9206@gmail.com>
2025-09-09 15:52:17 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical_chatbot",
+    version="0.0.0",
+    author="Anshika",
+    author_email="anshika9206@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
